7275b726dc251fd600edbe642181f61b775d2357
zenz-arun <arun@zenz.tech>
2025-10-17 19:41:49 +0530

    chore: updated the app.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Ennama Kannu savkiyama....</p>
+        <p>what bro? Its very wrong bro?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
